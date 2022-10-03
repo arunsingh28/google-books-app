26c31cf18f051dc1d97a1d9a1e2fb7055a4ac6d3
Arun Singh <arunsingh28aug.as@gmail.com>
2022-10-04 00:01:36 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+// import Router componets from app & Page folder
 
 test('renders learn react link', () => {
   render(<App />);
