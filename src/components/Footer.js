5fb577e32d4dc1f7284820d519dd82aa69b2b35f
arunsingh28 <arunsingh28aug.as@gmail.com>
2022-10-06 00:05:28 +0530

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,30 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <div>
+            <footer class="footer bg-primary mt-5">
+                <div class="container">
+                    <p className='text-center text-white'>Footer</p>
+                    <ul class="list-inline mb-0 text-center text-white">
+                        <li class="list-inline-item">
+                            <a>Link1</a>
+                        </li>
+
+                        <li class="list-inline-item">
+                            <a>Link1</a>
+                        </li>
+
+                        <li class="list-inline-item">
+                            <a>Link1</a>
+                        </li>
+
+                        <li class="list-inline-item">
+                            <a>Link1</a>
+                        </li>
+                    </ul>
+                </div>
+            </footer>
+        </div>
     )
 }
 
