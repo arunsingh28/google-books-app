bbdcc1a43ca33780cec2305015fd3fdf8bc9d398
arunsingh28 <arunsingh28aug.as@gmail.com>
2022-10-05 23:58:59 +0530

    home and login

---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+const Navbar = () => {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Books</a>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav ms-md-auto gap-2">
+                            <li className="nav-item rounded">
+                                <Link to="/" className="nav-link" id='navAtt-Home' aria-current="page"><i className="bi bi-house-fill me-2"></i>Home</Link>
+                            </li>
+                            <li className="nav-item rounded">
+                                <Link to="/register" className="nav-link" id='navAtt-Register' ><i className="bi bi-people-fill me-2"></i>Regsiter</Link>
+                            </li>
+                            <li className="nav-item rounded">
+                                <Link to={"/login"} className="nav-link" id='navAtt-Login'><i className="bi bi-telephone-fill me-2 bg-primary"></i>Login</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,23 +1,21 @@
-import { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
+import { Link } from 'react-router-dom'
 
-const Login = () => {
-
+const Register = () => {
     useEffect(() => {
         document.title = "Login"
         document.getElementById('navAtt-Home').classList.remove('active')
-        document.getElementById('navAtt-Register').classList.remove('active')
-        document.getElementById('navAtt-Login').classList.add('active')
+        document.getElementById('navAtt-Login').classList.remove('active')
+        document.getElementById('navAtt-Register').classList.add('active')
     }, [])
 
-
+    const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
-    const handleLogin = (e) => {
+    const handleRegister = (e) => {
         e.preventDefault()
-        console.log({ email, password })
     }
 
     return (
@@ -26,44 +24,52 @@ const Login = () => {
             <div class="global-container container px-5 mt-5">
                 <div class="card login-form">
                     <div class="card-body">
-                        <h3 class="card-title text-center">Login Books app</h3>
+                        <h3 class="card-title text-center">Register with Book App</h3>
                         <div class="card-text">
 
                             {/* message */}
                             {/* <div class="alert alert-danger alert-dismissible fade show" role="alert">Incorrect username or password.</div> */}
 
-                            <form onSubmit={handleLogin}>
-                                <div class="form-group">
+                            <form onSubmit={handleRegister} autoComplete="off">
+                                <div class="form-group mt-2">
+                                    <label htmlFor="name">Name</label>
+                                    <input type="text"
+                                        onChange={(e) => setName(e.target.value)}
+                                        value={name}
+                                        autoComplete="off"
+                                        className="form-control form-control-sm" id="name" aria-describedby="name" />
+                                </div>
+                                <div class="form-group mt-2">
                                     <label htmlFor="email">Email address</label>
                                     <input type="email"
                                         onChange={(e) => setEmail(e.target.value)}
                                         value={email}
+                                        autoComplete="off"
                                         className="form-control form-control-sm" id="email" aria-describedby="email" />
                                 </div>
                                 <div class="form-group mt-2">
                                     <label htmlFor="password">Password</label>
-                                    {/* <Link to="/forgot-password" style={{ float: "right", fontSize: "12px" }}>Forgot password?</Link> */}
                                     <input
                                         type="password"
                                         onChange={(e) => setPassword(e.target.value)}
                                         value={password}
+                                        autoComplete="off"
                                         className="form-control form-control-sm"
                                         id="password"
                                     />
                                 </div>
-                                <button type="submit" className="btn btn-primary btn-block mt-3">Sign in</button>
+                                <button type="submit" className="btn btn-primary btn-block mt-3">Register</button>
 
                                 <div class="sign-up mt-2">
-                                    Don't have an account? <Link to="/register">Create One</Link>
+                                    Already have an account? <Link to="/login">Login</Link>
                                 </div>
                             </form>
                         </div>
                     </div>
                 </div>
             </div>
-
         </div>
     )
 }
 
-export default Login
+export default Register
